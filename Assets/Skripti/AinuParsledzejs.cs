8395a33ca52d31af427d3c2f2d0d85a6be744e90
unknown <alekss.mozajevs@sk.lvt.lv>
2023-04-26 12:04:05 +0300

    izveidota sākuma aina un iespēja pārslēgties starp ainām

---
+++ Assets/Skripti/AinuParsledzejs.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AinuParsledzejs : MonoBehaviour {
+	public void UzSakumu(){
+		SceneManager.LoadScene ("Sakums", LoadSceneMode.Single);
+	}
+	public void UzUIelementi(){
+		SceneManager.LoadScene ("UI elementi", LoadSceneMode.Single);
+	}
+	public void apturet(){
+		Application.Quit ();
+		}
+	}
+
---
+++ Assets/Skripti/ObjektuParvietosana.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+
+
+public class ObjektuParvietosana : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	
+		private RectTransform transformKomponente;
+		public Canvas kanva;
+
+		void Start(){
+		transformKomponente=GetComponent<RectTransform>();
+		}
+
+	public void OnPointerDown(PointerEventData notikums){
+		Debug.Log ("Izdarīts peles klikšķis uz objekta!");
+	
+	}
+	public void OnBeginDrag(PointerEventData notikums){
+		Debug.Log ("Uzsākta objekta vilkšana!");
+	}
+	public void OnDrag(PointerEventData notikums){
+		Debug.Log ("Notiek objekta pārvietošana!");
+		transformKomponente.anchoredPosition += notikums.delta / kanva.scaleFactor;
+	}
+	public void OnEndDrag(PointerEventData notikums){
+		Debug.Log ("Pārtraukta objekta vilkšana!");
+	}
+	}
+
